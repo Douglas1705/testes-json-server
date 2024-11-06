07ad8a937c9c3a66e34fe664c746621a8464c97e
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-11-06 17:20:46 -0300

    Env

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -86,7 +86,7 @@ function SignUpPage() {
         </div>
         <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded">Cadastrar</button>
       </form>
-    </div>
+    </div>    
   );
 }
 
