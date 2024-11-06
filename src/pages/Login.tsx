22220f03f50f200c4df84633b112ece35d560369
Douglas1705 <bertoldi_douglas@hotmail.com>
2024-11-06 18:32:13 -0300

    Login buttons

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,41 @@
-import { SignIn } from '@clerk/clerk-react';
+import React from 'react';
+import { useSignIn } from '@clerk/clerk-react';
+import { RiFacebookCircleFill } from 'react-icons/ri'; 
+import { FcGoogle } from "react-icons/fc";
+
+const CustomSignInButton = ({ provider, icon }: { provider: string, icon: React.ReactNode }) => {
+  const { signIn } = useSignIn();
+
+  const handleSignIn = async () => {
+    try {
+      await signIn.authenticateWithRedirect({
+        strategy: provider,
+      });
+    } catch (error) {
+      console.error('Erro ao autenticar com', provider, ':', error);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleSignIn}
+      className="bg-white text-white rounded-full flex justify-center items-center w-auto h-16"
+    >
+      <div className="text-3xl text-blue-500 border-2 border-black px-8 py-3 rounded-full">
+        {icon}
+      </div>
+    </button>
+  );
+};
 
 function LoginPage() {
   return (
     <div className="container mx-auto my-8 p-8 border rounded-lg">
       <h2 className="text-2xl font-bold mb-4">Login</h2>
-      <SignIn
-        path="/login"
-        routing="path"
-        signUpUrl="/sign-up"
-        redirectUrl="/kanban"
-        appearance={{
-          elements: {
-            card: "shadow-lg p-8",
-            header: "text-2xl font-bold mb-4",
-            button: "w-full p-2 mt-4 bg-blue-500 text-white rounded",
-          },
-        }}
-      />
+      <div className="flex flex-row items-center space-x-4">        
+        <CustomSignInButton provider="oauth_facebook" icon={<RiFacebookCircleFill />} />
+        <CustomSignInButton provider="oauth_google" icon={<FcGoogle />} />
+      </div>
     </div>
   );
 }
